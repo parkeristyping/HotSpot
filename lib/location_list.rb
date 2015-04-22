2caf4c3cbe009c7a7095d61e5a0d65e9dab00bf4
Parker Lawrence <parker.alford@gmail.com>
2015-04-21 22:12:13 -0400

    start playing with styles

--- lib/location_list.rb
+++ lib/location_list.rb
@@ -4,12 +4,12 @@ class LocationList
   def self.create(locations)
     output = []
     locations.each_with_index {|location, index|
-      row = "[\'<div><h3>#{location.clean_name}</h3></div>"
+      row = "[\'<div class=\"loc-name pop-out\"><h2>#{location.clean_name}</h2></div>"
       location.posts.each {|post|
-        row << "<div class=\"post-wrap\">"
-        row << "<div class=\"post-thumb\"><img src=\"#{post.content_url}\"></div>"
-        row << "<div class=\"user\"><p>#{post.user.instagram_username}</p></div>"
-        row << "<div class=\"post-caption\"><p>#{post.clean_text}</p></div>"
+        row << "<div class=\"post-wrap pop-out\">"
+        row << "<div class=\"post-thumb pop-out\"><img src=\"#{post.content_url}\"></div>"
+        row << "<div class=\"user pop-out\"><p>#{post.user.instagram_username}</p></div>"
+        row << "<div class=\"post-caption pop-out\"><p>#{post.clean_text}</p></div>"
         row << "</div>"
       }
       row << "\', #{location.lat.round(6)}, #{location.lng.round(6)}, #{index}"
