f69818ab4c2274d7976a20a654dfbfee41a25b54
finvansant <ffparke@gmail.com>
2015-04-23 18:17:34 -0400

    add image assets, add css to map view

--- lib/location_list.rb
+++ lib/location_list.rb
@@ -4,15 +4,15 @@ class LocationList
   def self.create(locations)
     output = []
     locations.each_with_index {|location, index|
-      row = "[\'<div class=\"loc-name pop-out\"><h2>"
-      row << "<a href=\"#{location.url}\">" if location.url
+      row = "[\'<div class=\"loc-name pop-out\">"
+      row << "<a href=\" #{location.url} \" style=\"color:#4169E1; font-size: 25px;\">" if location.url
       row << "#{location.clean_name}"
       row << "</a>" if location.url
-      row << "</h2></div>"
+      row << "</div>"
       location.posts.each {|post|
         row << "<div class=\"post-wrap pop-out\">"
         row << "<div class=\"post-thumb pop-out\"><img src=\"#{post.content_url}\"></div>"
-        row << "<div class=\"user pop-out\"><p>#{post.user.instagram_username}</p></div>"
+        row << "<div class=\"user pop-out\"><p>user: #{post.user.instagram_username}</p></div>"
         row << "<div class=\"post-caption pop-out\"><p>#{post.clean_text}</p></div>"
         row << "</div>"
       }
@@ -23,3 +23,7 @@ class LocationList
     "[" + output.join(",") + "]"
   end
 end
+
+
+
+#### changes made
