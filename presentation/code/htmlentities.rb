1e8cb0c8b4a7256cd12626ddb3f1650dd435f499
Parker Lawrence <parker.alford@gmail.com>
2015-06-14 23:37:30 -0400

    remove presentation stuff

--- presentation/code/htmlentities.rb
+++
@@ -1,9 +0,0 @@
-# Cleaning for URL insertion
-require 'htmlentities'  
-ENCODER = HTMLEntities.new  
-options = {location_component: "location=#{lat},#{lng}",  
-               name_component: "name=#{ENCODER.encode(name, :named)}",
-               ...}
-
-# Here's an example of how it works...
-ENCODER.encode("root & bone", :named) #=> "root &amp; bone"
